4202fa4ea8cb0a5432dcbceae9ee5d11833bc591
Arnau Mora <arnyminer.z@gmail.com>
2022-02-21 20:15:30 +0100

    :bug: Fixed path `required` elements not being bool.

--- data_processing.js
+++ data_processing.js
@@ -14,8 +14,23 @@ module.exports = {
                 if (item.hasOwnProperty('sector'))
                     item['sector'] = item.sector.toString();
 
+                // Sector adjustments
                 if (item.hasOwnProperty('kidsApt'))
                     item['kidsApt'] = item.kidsApt === '1';
+
+                // Path adjustments
+                if (item.hasOwnProperty('crackerRequired'))
+                    item['crackerRequired'] = item.crackerRequired === '1';
+                if (item.hasOwnProperty('friendRequired'))
+                    item['friendRequired'] = item.friendRequired === '1';
+                if (item.hasOwnProperty('lanyardRequired'))
+                    item['lanyardRequired'] = item.lanyardRequired === '1';
+                if (item.hasOwnProperty('nailRequired'))
+                    item['nailRequired'] = item.nailRequired === '1';
+                if (item.hasOwnProperty('pitonRequired'))
+                    item['pitonRequired'] = item.pitonRequired === '1';
+                if (item.hasOwnProperty('stripsRequired'))
+                    item['stripsRequired'] = item.stripsRequired === '1';
 
                 items[objectId] = item;
             }
