54630d48e525614dfbfa4a0f25f99867c0230597
Arnau Mora <arnyminer.z@gmail.com>
2022-02-21 18:30:45 +0100

    :sparkles: Made `kidsApt` boolean.

--- data_processing.js
+++ data_processing.js
@@ -6,12 +6,17 @@ module.exports = {
                 const item = query[i];
                 const objectId = item.objectId.toString();
                 delete item["objectId"];
+
                 if (item.hasOwnProperty('area'))
                     item['area'] = item.area.toString();
                 if (item.hasOwnProperty('zone'))
                     item['zone'] = item.zone.toString();
                 if (item.hasOwnProperty('sector'))
                     item['sector'] = item.sector.toString();
+
+                if (item.hasOwnProperty('kidsApt'))
+                    item['kidsApt'] = item.kidsApt === '1';
+
                 items[objectId] = item;
             }
         return items;
